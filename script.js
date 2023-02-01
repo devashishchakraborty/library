f37aa9f891672a25fc38fbb9b24cfdfa60df4dbb
Devashish Chakraborty <devashishchakra@gmail.com>
2023-02-02 00:02:19 +0530

    Added event listener to toggle the visibility of form

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let myLibrary = [
     {
-        name: "Meditations",
+        title: "Meditations",
         author: "Marcus Aurelius",
         pages: 300
     },
     {
-        name: "Deep Work",
+        title: "Deep Work",
         author: "Cal Newport",
         pages: 250
     },
     {
-        name: "Beyond Good and Evil",
+        title: "Beyond Good and Evil",
         author: "Frederich Nietzsche",
         pages: 200
     },
     {
-        name: "How to Win Friends and Influence People.",
+        title: "How to Win Friends and Influence People.",
         author: "Dale Carnegie",
         pages: 240
     }
@@ -26,20 +26,33 @@ function Book() {
 }
 
 function addBookToLibrary() {
+    const addBook = document.querySelector(".add-book");
+    const form = document.querySelector("form");
 
+    addBook.addEventListener("click", function () {
+        form.setAttribute("class", "visible");
+    });
+
+    const formSubmit = form.querySelector("button");
+    formSubmit.addEventListener("click", function (event) {
+        const author = document.querySelector("input#author");
+        console.log(author.value);
+        // form.setAttribute("class", "hidden");
+        event.preventDefault();
+    });
 }
 
 function displayBooks() {
-    const body = document.querySelector("body")
+    const body = document.querySelector(".container")
     const grid = document.createElement("div");
     grid.setAttribute("class", "grid-container");
 
     for (let book of myLibrary) {
-        
+
         const gridItem = document.createElement("div");
         gridItem.setAttribute("class", "grid-item");
         grid.appendChild(gridItem);
-        
+
         for (let attribute in book) {
             const itemProperties = document.createElement("div");
             itemProperties.setAttribute("class", attribute)
@@ -51,3 +64,4 @@ function displayBooks() {
 }
 
 displayBooks();
+addBookToLibrary();
