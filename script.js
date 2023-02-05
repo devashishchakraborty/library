44f51ff34318a9215427c3a8a5b2087863cf9853
Devashish Chakraborty <devashishchakra@gmail.com>
2023-02-06 00:25:10 +0530

    Formatting text inside grid Item for each attribute. Also adding the Read and Not Read button for toggling later

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ let myLibrary = [
         name: "Meditations",
         author: "Marcus Aurelius",
         pages: 300,
-        read: "Yes"
+        read: true
     },
     {
         name: "Deep Work",
         author: "Cal Newport",
         pages: 250,
-        read: "No"
+        read: false
     },
     {
         name: "Beyond Good and Evil",
         author: "Frederich Nietzsche",
         pages: 200,
-        read: "Yes"
+        read: false
     },
     {
-        name: "How to Win Friends and Influence People.",
+        name: "How to Win Friends and Influence People",
         author: "Dale Carnegie",
         pages: 240,
-        read: "No"
+        read: true
     }
 ];
 
@@ -49,14 +49,14 @@ function addBookToLibrary() {
     const formClose = form.querySelector("button.close");
 
     formSubmit.addEventListener("click", function (event) {
-        const title = form.querySelector("input[name='title']");
+        const name = form.querySelector("input[name='name']");
 
         // Check if Form is Empty
         if (title.value.length !== 0 && author.value.length !== 0 && pages.value.length !== 0) {
-            const book = new Book(title.value, author.value, pages.value, read.value);
+            const book = new Book(name.value, author.value, pages.value, read.value);
             myLibrary.push(book);   // Adding the Object to library array
 
-            title.value = "";
+            name.value = "";
             author.value = "";
             pages.value = "";
             overlay.classList.add("hidden");    // Hide the form
@@ -89,8 +89,25 @@ function displayBooks() {
 
         for (let attribute in book) {
             const itemProperties = document.createElement("div");
-            itemProperties.setAttribute("class", attribute)
-            itemProperties.textContent = `${attribute.charAt(0).toUpperCase() + attribute.slice(1)}: ${book[attribute]}`;
+            itemProperties.setAttribute("class", attribute);
+            // Formatting text in gridItem for each attribute
+            if (attribute === "name"){
+                itemProperties.textContent = `"${book[attribute]}"`;
+            } else if (attribute === "author") {
+                itemProperties.textContent = `by ${book[attribute]}`;
+            } else if (attribute === "pages") {
+                itemProperties.textContent = `${book[attribute]} pages`;
+            } else if (attribute === "read"){   // Creating the read button
+                const readButton = document.createElement("button");
+                if (book[attribute]){
+                    readButton.setAttribute("read", "true")
+                    readButton.textContent = "Read";
+                } else{
+                    readButton.setAttribute("read", "false")
+                    readButton.textContent = "Not Read";
+                }
+                itemProperties.appendChild(readButton);
+            }
             gridItem.appendChild(itemProperties);
         }
         body.appendChild(grid);
@@ -109,7 +126,7 @@ function displayDescription() {
     }, 0);
 
     let readCount = myLibrary.reduce(function (count, book) {
-        if (book.read === "Yes") count++;
+        if (book.read) count++;
         return count;
     }, 0);
 
